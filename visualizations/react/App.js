2bae8844e4e6c63e2e80892ced3acaaf917b37dd
nrrbrto <moran.nrbrto@gmail.com>
2025-02-27 23:12:05 -0800

    Added tons of files and structure changes. Completed and revised raw and processed data

---
+++ visualizations/react/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CombinedVisualizations from "./CombinedVisualizations";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CombinedVisualizations />
+    </div>
+  );
+}
+
+export default App;
